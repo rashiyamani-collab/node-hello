64e69d475688bc5b8c071264c45578f533d22abd
rashiyamani-collab <97337399+rashiyamani-collab@users.noreply.github.com>
2024-09-06 22:07:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Node Rashiyamani first Azure project!\n'
+  const msg = 'Hello Node Rashiyamani first Azure project Version 2!\n'
   res.end(msg);
 });
 
